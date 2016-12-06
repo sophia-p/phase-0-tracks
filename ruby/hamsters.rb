287e5366661eb4358456a679b3a5d73c4bb56ad3
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-06 12:39:54 -0500

    edit questionnaire

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -4,10 +4,15 @@ puts "What is the hamster's volume level, from 1 to 10?"
 hamster_volume=gets.chomp.to_f
 puts "What is the hamster's fur color?"
 color_fur=gets.chomp
-puts "Is the hamster a good candidate for adoption?"
+puts "Is the hamster a good candidate for adoption? Y/N"
 adoption=gets.chomp
-puts "What is the estimate age?"
+	if adoption == Y
+		
+puts "What is the estimated age?"
 age= gets.chomp.to_i
 	if age == ""
 		age == nil
 	end
+
+print "#{hamster_name} has a volume of #{hamster_volume} and #{color_fur} fur.
+#{hamster_name} is a(n) #{adoption}"
