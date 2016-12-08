e162cd6ddf931bd8d2dbb2193fbc114dde9d22d5
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-08 15:56:16 -0500

    add and test case statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,22 +8,42 @@ puts "What year were you born?"
 yearmatch = true
 byear = gets.chomp.to_i
 	if (2016-byear) == age
-		yearmatch
+		yearmatch 
+	else
+		yearmatch = false
 	end
 
 puts "Our company cafeteria serves garlic bread.
 Should we order some for you?"
-garlic = gets.chomp
-	if garlic == "yes".upcase || garlic == "yes".capitalize! || garlic == "yes"
-		true
+	order_g = gets.chomp
+	if order_g == "yes".upcase || order_g == "yes".capitalize! || order_g == "yes"
+		order_g=true
 	else 
-		false
+		order_g=false
 	end
 
 puts "Would you like to enroll in the company's health insurance?"
-health = gets.chomp
+	health = gets.chomp
+	if health == "yes".upcase || health == "yes".capitalize! || health == "yes"
+		health = true
+	else 
+		health = false
+	end
 
-if yearmatch && garlic
-	print "Not a vampire"
-end
+
+	case 
+	when name == "Drake Cula" || name == "Tu Fang" ||name == "Nosferatu"
+		print "Definitely a vampire"	
+	when yearmatch && (order_g || health)
+		print "Probably not a vampire."
+	when !yearmatch && !order_g && !health
+		print "Almost certainly a vampire."
+	when !yearmatch && (!order_g || !health)
+		print "Probably a vampire."
+	else 
+		puts "Inconclusive."
+
+	end
+
+
 
