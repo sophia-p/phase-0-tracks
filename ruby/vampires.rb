ef5e2fd54d5af2cb55e3facfa84559b141f17d3f
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-08 10:31:38 -0500

    start vampire questionnaire

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+byear = gets.chomp
+
+puts "Our company cafeteria serves garlic bread.
+Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+health = gets.chomp
