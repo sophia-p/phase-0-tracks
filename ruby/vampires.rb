c32ec83b0ef9e26fc39eb9f0fc91256f95458fb4
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-08 11:00:35 -0500

    add conditionals for age and garlic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,14 +2,28 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "What year were you born?"
-byear = gets.chomp
+yearmatch = true
+byear = gets.chomp.to_i
+	if (2016-byear) == age
+		yearmatch
+	end
 
 puts "Our company cafeteria serves garlic bread.
 Should we order some for you?"
 garlic = gets.chomp
+	if garlic == "yes".upcase || garlic == "yes".capitalize! || garlic == "yes"
+		true
+	else 
+		false
+	end
 
 puts "Would you like to enroll in the company's health insurance?"
 health = gets.chomp
+
+if yearmatch && garlic
+	print "Not a vampire"
+end
+
