d157952bb39b1d3a2b294afcf1e86eb18ab51912
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-14 18:49:46 -0500

    create questionnaire and store in hash

---
+++ ruby/hashes.rb
@@ -0,0 +1,51 @@
+=begin
+-Prompt user for info on name (string), age (integer), 	
+number of children (integer), decor theme (string), budget (integer), based on game MASH (boolean)
+-Convert data to appropriate type and store data in hash
+-Print when done
+-Ask if user needs to update
+	-if none, skip
+	-if user types a key, ask for new value and update key
+-Print finished version, exit program
+=end
+
+interior_dec = {}
+
+puts "What is your name?"
+app_name = gets.chomp
+interior_dec[:name] = app_name
+
+puts "What is your age?"
+app_age = gets.chomp.to_i
+interior_dec[:age] = app_age
+
+puts "How many children do you have?"
+app_children = gets.chomp.to_i
+interior_dec[:children] = app_children
+
+puts "What is your decor theme?"
+app_theme = gets.chomp
+interior_dec[:decor_theme] = app_theme
+
+puts "What is your budget?"
+app_budget = gets.chomp.to_f
+interior_dec[:budget] = app_budget
+
+puts "Are you living your life based on the game MASH?"
+app_mash = gets.chomp
+	if app_mash == "yes".upcase || app_mash == "yes".capitalize! || app_mash == "yes"
+		app_mash = true #set to true
+	else 
+		app_mash = false
+	end
+	interior_dec[:mash] = app_mash
+
+p interior_dec
+puts "If you do not need to make any changes, please type 'none'. 
+If you need to make a change, please type the category name."
+	if gets.chomp = "name"
+		puts "Type your name"
+		app_name = gets.chomp
+		interior_dec[:name] = app_name
+	end
+p interior_dec
