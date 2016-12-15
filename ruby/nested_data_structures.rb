32a374ae99091dd2420066dca440d5206e7d66df
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-15 00:36:07 -0500

    add fancy and fish shack to restaurant hash

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -18,7 +18,7 @@ restaurants = {
 		]
 
 	},
-	pizza = {
+	pizza: {
 		location: 'New York, NY, USA',
 		average_prices: {
 			appetizers: 10,
@@ -34,6 +34,34 @@ restaurants = {
 		]
 
 	},
+	fancy: {
+		location: 'London, UK',
+		average_prices: {
+			appetizers: 20,
+			mains: 38,
+			desserts: 15
+		},
+		most_used_ingredients: [
+			'dover sole',
+			'beef',
+			'fennel',
+			'cream',
+		]
+	},
+	fish_shack: {
+		location: 'Newport, RI, USA',
+		average_prices: {
+			appetizers: 12,
+			mains: 26,
+			desserts: 10
+		},
+		most_used_ingredients: [
+			'lobster',
+			'oysters',
+			'crabs',
+			'clams',
+		]
+	},	
 
 
 }
