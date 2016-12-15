0362981bf169a20a29099274732ea970398e52e9
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-15 00:30:48 -0500

    add bistro and pizza to restaurant nested hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,39 @@
+
+
+restaurants = {
+	bistro: {
+		location: 'Paris, France',
+		average_prices: {
+			appetizers: 10,
+			mains: 27,
+			desserts: 12,
+		},
+		most_used_ingredients: [
+			'wine',
+			'chicken',
+			'beef',
+			'onion',
+			'carrots',
+			'butter'
+		]
+
+	},
+	pizza = {
+		location: 'New York, NY, USA',
+		average_prices: {
+			appetizers: 10,
+			mains: 27,
+			desserts: 8 
+		},
+		most_used_ingredients: [
+			'garlic',
+			'tomato',
+			'onion',
+			'flour',
+			'olive oil',
+		]
+
+	},
+
+
+}
