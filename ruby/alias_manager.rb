8c10c3fa37bd5f063a50264d6fb05568a985704e
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-16 01:02:59 -0500

    test and copy code from repl

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,14 +1,41 @@
 =begin
 
 -split name into an array of 2 names and reverse the order
+-downcase the names
 -store 	vowels and consonants as variables
--create separate method to check if letter is vowel or consonant
+-in same method as vowel and consonant variables, 
+	check if letter is vowel or consonant
 	and change to the next letter
 -use map to change letter to the next one by calling the letter checker method
-
-	
+-use capitalize method to capitalize the first letters of the names
+-join name back together	
 =end
+def next_vowcon(oldlet) 
+  vowel = ['a','e','i','o','u']
+  rot_vow = vowel.rotate
+  consonant = 'bcdfghjklmnpqrstvwxzy'.split('')
+  rot_cons= consonant.rotate
+  i=0
+  while i < oldlet.length
+    if vow_index = vowel.index(oldlet[i])
+      oldlet[i] = rot_vow[vow_index]
+    else
+      cons_index = consonant.index(oldlet[i])
+      oldlet[i] = rot_cons[cons_index]
+    end
+  i +=1
+  end
+  return oldlet
+end
 
-def spy_name(name)
-	name = name.split(' ').reverse
+
+def name_changer(prevname)
+  prevname = prevname.downcase.split(' ').reverse
+  prevname.map!{|undercover|
+    next_vowcon(undercover)
+  }
+  newname = prevname.map{|profession|
+    profession.capitalize!
+  }
+  return newname.join(' ')
 end
