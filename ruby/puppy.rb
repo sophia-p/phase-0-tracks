ff2096c1adc77780d89da3137a3c07fab9dedc7b
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-20 09:50:59 -0500

    pseudocode puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# PET ------------------------
+# Dog
+
+# CHARACTERISTICS ----------------
+# Age: <1
+# Fur softness: high
+# Floppy ears: yes
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------------
+# Run
+# Chew
+# Sleep
