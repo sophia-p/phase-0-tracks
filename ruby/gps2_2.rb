354fb7cf3a705d811211afe483eb01b418ac138e
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-19 21:18:47 -0500

    pseudocode hash populator

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,29 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # create an empty and hash that we will populate with items and quantities
+  # string.split(' ') to turn it into an array
+  # use loop/iteration to set each array item as a key with its default quantity (1?) as a value
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: Hashes with keys of item and values of quantity
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
