468a620667f81d3818197d9cd29286615d2c0c93
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-21 22:56:01 -0500

    add 2 methods to module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+	def self.yell_angrily(words)
+		words + '!!!' + ' :('
+	end
+
+	def self.yell_happily(words)
+		'OMG ' + words.upcase + ' YESSSSSS'
+	end
 end
+
+p Shout.yell_angrily("Uuuuuugh")
+p Shout.yell_happily("It's Caturday")
