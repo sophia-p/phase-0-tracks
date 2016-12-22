41e0bea98ad1db279d80ca237f7016e06b939c00
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-21 23:24:00 -0500

    make mixin version of Shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,34 @@
-module Shout
-	def self.yell_angrily(words)
-		words + '!!!' + ' :('
-	end
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + '!!!' + ' :('
+# 	end
 
-	def self.yell_happily(words)
-		'OMG ' + words.upcase + ' YESSSSSS'
+# 	def self.yell_happily(words)
+# 		'OMG ' + words.upcase + ' YESSSSSS'
+# 	end
+# end
+
+# p Shout.yell_angrily("Uuuuuugh")
+# p Shout.yell_happily("It's Caturday")
+
+module Shout
+	def yell(words)
+		puts "#{words.upcase}!!!"
 	end
 end
 
-p Shout.yell_angrily("Uuuuuugh")
-p Shout.yell_happily("It's Caturday")
+class Airline_Passenger
+	include Shout
+end
+
+class DMV_Customer
+	include Shout
+end
+
+airline_passenger = Airline_Passenger.new
+airline_passenger.yell("5 hour delay?")
+
+dmv_customer = DMV_Customer.new
+dmv_customer.yell("I've been waiting for 3 hours")
+
+
