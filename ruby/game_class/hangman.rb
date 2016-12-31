b243b885892f1762f20ddadff9e62de244e3ccd5
Sophia Pignatelli <pignatelli@fordham.edu>
2016-12-30 22:06:39 -0500

    use loop for user guesses

--- ruby/game_class/hangman.rb
+++ ruby/game_class/hangman.rb
@@ -1,23 +1,24 @@
 class Hangman
-	attr_accessor :given_word, :guess_letter
+	attr_accessor :given_word, :guess_letter, :guess_counter
 	attr_reader :dash_word
 	def initialize(given_word)
 		@given_word = given_word
+		@guess_counter = given_word.length
 	end
 
 	def guesser(guess_letter)
 		@guess_letter = guess_letter
 		if @given_word.include? @guess_letter
-			@dash_word = @dash_word.split(' ')
 			index = @given_word.index(@guess_letter)
 			@guessing_word = @dash_word.delete_at(index)
-			@guessing_word = @dash_word.insert(index, @guess_letter)
+			@guessing_word = @dash_word.insert(index, @guess_letter).join(' ')
+		else
+			@dash_word
 		end
-		return @guessing_word.join(' ')
 	end
 
 	def check_word
-		@dash_word = (@given_word.gsub(/\w/,'- ')).rstrip!
+		@dash_word = (@given_word.gsub(/\w/,'- ')).rstrip!.split(' ')
 	end
 
 end
@@ -25,13 +26,18 @@ end
 
 #user interface
 
-puts "Hello, User One, please type word for User Two to guess."
+puts "Hello, User One, please type a word for User Two to guess."
 game = Hangman.new(gets.chomp)
 
-puts "Hello, User Two, your word is #{game.check_word}. Start by guessing a letter"
-guess = game.guesser(gets.chomp)
+ 
+puts "Hello, User Two, your word is #{game.check_word}"
+while game.guess_counter > 0
+	puts "Guess a letter"
+	guess = game.guesser(gets.chomp)
+	puts "#{guess}"
+	game.guess_counter -=1
+end
 
-puts "Your word is now #{guess}"
 
 
 
