08153dc184a953cd2cd69c93cd6924758507aee1
Sophia Pignatelli <pignatelli@fordham.edu>
2017-01-03 20:56:49 -0500

    write reverseString method

---
+++ js/explore.js
@@ -0,0 +1,14 @@
+/*
+use a for loop to start at the end of the string and add each letter before it to the end of it
+so, if the string is "hello", start at the end of the string with 'o', and then add the 'l'
+and so on with concatenation. Assign the reversed string to a new variable*/
+
+function reverseString(string) {
+	var len= string.length -1;
+	var answerString = "";
+	for(var i = len; i >=0; i++){
+		answerString += string[i];
+	}
+}
+
+answerString("hello")
