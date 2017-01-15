0efb76b0d0128c12b8e90ae54da75aab5707d949
Sophia Pignatelli <pignatelli@fordham.edu>
2017-01-15 16:59:42 -0500

    test code

--- databases/something_awesome/movie_rate.rb
+++ databases/something_awesome/movie_rate.rb
@@ -8,7 +8,7 @@ db = SQLite3::Database.new("movies.db")
 
 
 create_table_cmd = <<-SQL
-  CREATE TABLE movies(
+  CREATE TABLE IF NOT EXISTS movies(
     id INTEGER PRIMARY KEY,
     title VARCHAR(255),
     year INT,
@@ -17,4 +17,24 @@ create_table_cmd = <<-SQL
 SQL
 
 db.execute(create_table_cmd)
+# db.results_as_hash = true
+
+
+movies = db.execute("SELECT * FROM movies")
+
 # db.execute("INSERT INTO movies (title, year, rating) VALUES ('Casablanca', 1942, 5)")
+
+
+
+
+puts "Enter a movie title:"
+movie_title = gets.chomp
+puts "And its release year:"
+movie_year = gets.chomp.to_i
+puts "And rate the movie out of 5:"
+movie_rating = gets.chomp.to_i
+
+movie_info = movie_title, movie_year, movie_rating
+def create_movie(db, title, year,rating)
+  db.execute("INSERT INTO movies (title, year, rating) VALUES (?, ?, ?)", [movie_info])
+end
