9f684e829422bce75c2a10070324bad9c4f8947c
Sophia Pignatelli <pignatelli@fordham.edu>
2017-01-15 11:28:08 -0500

    Add movies table

--- databases/something_awesome/movie_rate.rb
+++ databases/something_awesome/movie_rate.rb
@@ -7,15 +7,14 @@ require 'sqlite3'
 db = SQLite3::Database.new("movies.db")
 
 
+create_table_cmd = <<-SQL
+  CREATE TABLE movies(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255),
+    year INT,
+    rating INT
+  )
+SQL
 
-# create_table_cmd = <<-SQL
-#   CREATE TABLE IF NOT EXISTS movies(
-#     id INTEGER PRIMARY KEY,
-#     title VARCHAR(255),
-#     year INT,
-#     rating INT,
-#   )
-# SQL
-
-# db.execute(create_table_cmd)
+db.execute(create_table_cmd)
 # db.execute("INSERT INTO movies (title, year, rating) VALUES ('Casablanca', 1942, 5)")
