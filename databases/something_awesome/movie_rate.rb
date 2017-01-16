746c56f6944939cefe0c4d73d5e5e826a5d0b151
Sophia Pignatelli <pignatelli@fordham.edu>
2017-01-15 20:57:41 -0500

    clean up code a bit and comment

--- databases/something_awesome/movie_rate.rb
+++ databases/something_awesome/movie_rate.rb
@@ -2,11 +2,10 @@
 require 'sqlite3'
 
 # create SQLite3 database 
-	# (the equivalent of sqlite3 kittens.db on command line)
 	# store in variable
 db = SQLite3::Database.new("movies.db")
 
-
+# create the table if it doesn't already exist using fancy string delimiters
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS movies(
     id INTEGER PRIMARY KEY,
@@ -15,30 +14,19 @@ create_table_cmd = <<-SQL
     rating INT
   )
 SQL
-
+# execute the create table command
 db.execute(create_table_cmd)
-# db.results_as_hash = true
 
-
+# movies variable contains all the movie information
 movies = db.execute("SELECT * FROM movies")
 
-# db.execute("INSERT INTO movies (title, year, rating) VALUES ('Casablanca', 1942, 5)")
 
-
-
-
-# puts "Enter a movie title:"
-# movie_title = gets.chomp
-# puts "And its release year:"
-# movie_year = gets.chomp.to_i
-# puts "And rate the movie out of 5:"
-# movie_rating = gets.chomp.to_i
-
-
+# create method that puts the info into the database
 def create_movie(db, title, year,rating)
   db.execute("INSERT INTO movies (title, year, rating) VALUES (?, ?, ?)", [title,year,rating])
 end
 
+# call the method using the wanted arguments
 puts "How many movies would you like to add?"
 done = gets.chomp.to_i
 until done == 0 do
