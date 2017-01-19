eba5f9ea1872e1eeab6d2d5a2a5f133e2e016d09
Sophia Pignatelli <pignatelli@fordham.edu>
2017-01-19 16:57:59 -0500

    create add new course form

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,9 +7,15 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
+  erb :home
+end
+
+get '/' do
+  @courses = db.execute("SELECT * FROM courses")
   erb :home
 end
 
@@ -17,11 +23,22 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/courses' do
+  erb :courses
+end
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
+end
+
+# create new course form
+post '/students' do
+	db.execute("CREATE TABLE courses (title, level, id)")
+	db.execute("INSERT INTO courses (title, level, id) VALUES (?,?,?)", [params['title'], params['level'], params['id'].to_i])
+	redirect '/'
 end
 
 # add static resources
